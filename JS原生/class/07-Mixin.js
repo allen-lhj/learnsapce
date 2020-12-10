ce40a76656bbc307d3085c20e18f406390fd1072
allen-lhj <liu772636821@163.com>
2020-12-10 10:41:27 +0800

    definedProperty

---
+++ JS原生/class/07-Mixin.js
@@ -0,0 +1,13 @@
+const mixins = {
+    a: 'a',
+    b: 'b'
+}
+// function mix (...mixins) {
+//     class Mix {
+//         constructor() {
+//             for (let mixin of mixins) {
+//
+//             }
+//         }
+//     }
+// }
---
+++ JS原生/对象/defineProperty.js
@@ -0,0 +1,16 @@
+const object1 ={}
+
+// Object.defineProperty(obj：要定义属性的对象, prop：要定义或修改的属性的名称或 Symbol , descriptor：要定义或修改的属性描述符。)
+Object.defineProperty(object1, 'property1', {
+    value: 42,
+    writable: false
+})
+
+object1.property1 = 77; // 设置了writable:false throws an error in strict mode
+
+console.log(object1.property1)
+
+// 该方法允许精确地添加或修改对象的属性。通过赋值操作添加的普通属性是可枚举的，
+// 在枚举对象属性时会被枚举到（for...in 或 Object.keys 方法），可以改变这些属性的值，
+// 也可以删除这些属性。这个方法允许修改默认的额外选项（或配置）。默认情况下，使用 Object.defineProperty()
+// 添加的属性值是不可修改（immutable）的。
