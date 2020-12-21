b67e0d35b2f0dd193ffab9a985d96fe01f034657
allen-lhj <liu772636821@163.com>
2020-12-22 00:58:09 +0800

    vue3.0

---
+++ Vue/vue3demo/src/components/User.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>username: {{username}}</h1>
+    <h1>age: {{age}}</h1>
+    <h1>description: {{description}}</h1>
+    <h1>姓名：{{name}}</h1>
+    <h1>班级：{{classn}}</h1>
+  </div>
+</template>
+
+<script>
+import {ref, onMounted, inject} from 'vue'
+export default {
+name: "User",
+  props: ['username', 'age'],
+  setup(props) {
+    console.log(props)
+    const description = ref(props.username + '年龄是： '+ props.age)
+    onMounted(() => {
+      console.log('setup Mounted')
+    })
+    const student = inject('student')
+    return {
+      description,
+      ...student
+    }
+  },
+  created() {
+    console.log('create')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
