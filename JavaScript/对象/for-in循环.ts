b19e9d98b678cef8ad329b8374d48a857f8dac90
allen-lhj <liu772636821@163.com>
2021-03-30 14:03:24 +0800

    fix files

--- JavaScript/对象/for-in循环.ts
+++
@@ -1,7 +0,0 @@
-// for-in 循环遍历对象的属性
-let txt: string = ''
-let person: object = {fname: 'Bill', lname: 'Gates', age: 62}
-for (let i in person) {
-    txt += person[i] + ' '
-}
-console.log(txt)
--- JavaScript/对象/object/objectassign.js
+++
@@ -1,7 +0,0 @@
-const target1 = { a: 1 }
-const target2 = { b: 2 }
-const target3 = { a: 1 }
-
-Object.assign(target1, target2, target3)
-console.log(target1)
-
